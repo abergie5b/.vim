b825404a46b23a656f14d9d6cf640a1e1a3cc866
Andrew Berger <abberger1@gmail.com>
2016-08-13 18:02:01 -0500

    added environment setup file

---
+++ envirn.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+sudo apt-get update
+sudo apt-get install vim
+sudo apt-get install git-core
+mkdir $HOME/src
+mkdir $HOME/src/c++
+mkdir $HOME/src/python
+echo "export PATH=$HOME/src/"
+curl -o Anaconda3-4.1.1.sh http://repo.continuum.io/archive/Anaconda3-4.1.1-Linux-x86_64.sh
+bash Anaconda3-4.1.1.sh
+alias python=$HOME/anaconda3/bin/python3.5
+alias mv="mv --backup=numbered"
+alias rm="mv --backup=numbered --target-directory=C:/users/andrew/.trash"
