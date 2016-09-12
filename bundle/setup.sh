1e0fe093a1ef5b4d7240fd2062e74e96d365495e
Andrew <aberger91@protonmail.com>
2016-09-12 22:17:34 +0000

    moved autoload to .vim

--- bundle/setup.sh
+++ bundle/setup.sh
@@ -3,3 +3,4 @@ git clone https://github.com/majutsushi/tagbar
 git clone https://github.com/tpope/vim-pathogen
 git clone https://github.com/PyCQA/pycodestyle
 git clone https://github.com/scrooloose/syntastic
+mv $HOME/.vim/bundle/vim-pathogen/autoload $HOME/.vim/
