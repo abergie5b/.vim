8621aba5cd0ff174665d22481c3b6cdc977d2e0a
Andrew <aberger91@protonmail.com>
2016-09-12 22:16:30 +0000

    added exports/aliases to bashrc

--- envirn.sh
+++ envirn.sh
@@ -8,7 +8,7 @@ mkdir $HOME/src/c++
 mkdir $HOME/src/python
 mkdir $HOME/tmp
 mkdir $HOME/docs
-echo "export PATH=$HOME/src/"
-alias python=$HOME/anaconda3/bin/python3.5
-alias mv="mv --backup=numbered"
-alias rm="mv --backup=numbered --target-directory=C:/users/andrew/.trash"
+echo "export PATH=$PATH:/$HOME/src/" >> $HOME/.bashrc
+echo "alias python=$HOME/anaconda3/bin/python3.5" >> $HOME/.bashrc
+echo "alias mv="mv --backup=numbered"" >> $HOME/.bashrc
+echo "alias rm="mv --backup=numbered--target-directory=C:/users/andrew/.trash"" >> $HOME/.bashrc
