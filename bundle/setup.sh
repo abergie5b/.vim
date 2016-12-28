e4bc934ab72113f91082b6076fd0f63b8aa35b73
Andrew <aberger91@protonmail.com>
2016-12-28 10:47:39 -0600

    added airline themes

--- bundle/setup.sh
+++ bundle/setup.sh
@@ -3,4 +3,6 @@ git clone https://github.com/majutsushi/tagbar
 git clone https://github.com/tpope/vim-pathogen
 git clone https://github.com/PyCQA/pycodestyle
 git clone https://github.com/scrooloose/syntastic
+git clone https://github.com/vim-airline/vim-airline
+git clone https://github.com/vim-airline/vim-airline-themes
 mv $HOME/.vim/bundle/vim-pathogen/autoload $HOME/.vim/
