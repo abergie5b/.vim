4bcf0d898f8fafce2002794c8dd0c5a2680c9e32
Andrew Berger <aberger91@protonmail.com>
2017-02-24 16:58:02 -0600

    removed mkdir and export lines

--- envirn.sh
+++ envirn.sh
@@ -1,12 +1,4 @@
 #!/bin/bash
-sudo apt-get install vim
 ln -s ~/.vim/.vimrc ~/.vimrc
 sudo apt-get install curl
 sudo apt-get install exuberant-ctags
-mkdir $HOME/src
-mkdir $HOME/src/c++
-mkdir $HOME/src/python
-mkdir $HOME/tmp
-mkdir $HOME/docs
-echo "export PATH=$PATH:/$HOME/src/:$HOME/anaconda3/bin/:" >> $HOME/.bashrc
-echo "alias python=$HOME/anaconda3/bin/python3.5" >> $HOME/.bashrc
