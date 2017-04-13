e4ede0f78efffb8849fcda8950d658f5bade5961
Andrew Berger <aberger91@protonmail.com>
2017-04-12 21:20:25 -0500

    added mv .vimrc ~/

--- configure.sh
+++ configure.sh
@@ -3,3 +3,4 @@ sudo apt-get install exuberant-ctags
 git clone https://github.com/aberger91/.vim
 wget http://repo.continuum.io/archive/Anaconda3-4.3.0-Linux-x86_64.sh
 git clone https://github.com/aberger91/tserie
+mv .vimrc ~/
