dd5703fab86e454874af52f4aefddffccfdf2b95
Andrew <abberger1@gmail.com>
2017-11-12 14:12:45 -0600

    cleaned up some stuff

--- configure.sh
+++ configure.sh
@@ -1,6 +1,3 @@
+#!/bin/bash
 sudo apt-get install vim
 sudo apt-get install exuberant-ctags
-git clone https://github.com/aberger91/.vim
-wget http://repo.continuum.io/archive/Anaconda3-4.3.0-Linux-x86_64.sh
-git clone https://github.com/aberger91/tserie
-mv .vimrc ~/
