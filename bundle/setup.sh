e05a1d6d4aa08a28dff466a01c4252eadda5eb3c
aberger91 <aberger91@protonmail.com>
2017-11-29 18:11:03 -0600

    added depth=1 wtc

--- bundle/setup.sh
+++ bundle/setup.sh
@@ -7,5 +7,5 @@ git clone https://github.com/vim-airline/vim-airline
 git clone https://github.com/vim-airline/vim-airline-themes
 git clone https://github.com/scrooloose/nerdtree
 git clone https://github.com/ryanoasis/vim-devicons
-git clone https://github.com/ryanoasis/nerd-fonts
+git clone --depth 1 https://github.com/ryanoasis/nerd-fonts
 cp -r vim-pathogen/autoload ../
