f80e796f3b5f998a55ba04ccd81bedb43b3c83a0
aberger91 <aberger91@protonmail.com>
2017-11-29 18:15:08 -0600

    installing Hack as default font

--- bundle/setup.sh
+++ bundle/setup.sh
@@ -8,4 +8,7 @@ git clone https://github.com/vim-airline/vim-airline-themes
 git clone https://github.com/scrooloose/nerdtree
 git clone https://github.com/ryanoasis/vim-devicons
 git clone --depth 1 https://github.com/ryanoasis/nerd-fonts
+cd nerd-fonts
+./install.sh Hack
+cd $HOME
 cp -r vim-pathogen/autoload ../
